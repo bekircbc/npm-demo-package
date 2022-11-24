29c6f37ddc80cf2d985cca82742048be011b2a1c
Bekir - CDI <b.burakcebecii@gmail.com>
2022-11-25 00:36:54 +0100

    updated

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ import lodash from "lodash";
 exports.printMsg = function () {
   console.log("This message is from the npm-demo-pkg");
 };
+
+exports.isWds = function (string) {
+  return string === "WDS";
+};
